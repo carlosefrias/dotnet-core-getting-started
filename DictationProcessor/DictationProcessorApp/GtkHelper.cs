3ff3b190f718e95942612f95f7bdb50f38cb868b
Carlos Frias <carlosefrias@gmail.com>
2019-09-15 14:18:32 +0100

    end

---
+++ DictationProcessor/DictationProcessorApp/GtkHelper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace DictationProcessorApp
+{
+    public class GtkHelper
+    {
+        [DllImport("libgtk-x11-2.0.so.0")]
+        static extern void gtk_init(int argc, IntPtr argv);
+
+        [DllImport("libgtk-x11-2.0.so.0")]
+        static extern IntPtr gtk_message_dialog_new(IntPtr window, int flags, int type, int buttons, string message,
+            IntPtr args);
+
+        [DllImport("libgtk-x11-2.0.so.0")]
+        static extern int gtk_dialog_run(IntPtr dialog);
+
+        public static void DisplayAlert(string message)
+        {
+            gtk_init(0, IntPtr.Zero);
+            var dialog = gtk_message_dialog_new(IntPtr.Zero, 0, 0, 1, message, IntPtr.Zero);
+            gtk_dialog_run(dialog);
+        }
+    }
+}
--- DictationProcessor/DictationProcessorApp/Program.cs
+++ DictationProcessor/DictationProcessorApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Runtime.InteropServices;
 using DictationProcessorLib;
 
 namespace DictationProcessorApp
@@ -7,11 +8,19 @@ namespace DictationProcessorApp
     {
         static void Main(string[] args)
         {
+            var i = 0;
             //iterate through subfolders of /mnt/uploads
             foreach (var subfolder in Directory.GetDirectories("../../../../uploads"))
             {
                 var uploadProcessor = new UploadProcessor(subfolder);
                 uploadProcessor.Process();
+                i++;
+            }
+
+            var message = $"{i} uploads were processed";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                GtkHelper.DisplayAlert(message);
             }
         }
     }
